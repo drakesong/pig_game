d9d0ec4c6b0a145ce012e0ef07b8a517bf130ac3
Drake Song <drakesong216@gmail.com>
2018-02-18 02:36:04 -0800

    Added more features to the game

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var scores, activePlayer, roundScore, gamePlaying, lastDice;
+var scores, activePlayer, roundScore, gamePlaying;
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
-        var dice = Math.floor(Math.random() * 6) + 1;
-        var diceDOM = document.querySelector('.dice');
+        var dice0 = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
 
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-0').style.display = 'block';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-0').src = 'dice-' + dice0 + '.png';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
 
-        if (dice === 6 && lastDice === 6) {
+        if (dice0 === 6 && dice1 === 6) {
             scores[activePlayer] = 0;
             document.querySelector('#score-' + activePlayer).textContent = '0';
             nextPlayer();
-        } else if (dice !== 1) {
-            roundScore += dice;
+        } else if (dice0 !== 1 && dice1 !== 1) {
+            roundScore += dice0 + dice1;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             nextPlayer();
         }
-
-        lastDice = dice;
     }
 });
 
@@ -41,7 +41,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
         if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-0').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -61,7 +62,8 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-0').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -72,7 +74,8 @@ function init() {
     roundScore = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-0').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
 
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
